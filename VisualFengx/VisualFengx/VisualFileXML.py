c1c454ff3c2667c653617a780949c787ca077cfd
fengx <zengme@gmail.com>
2014-03-31 19:29:02 +0800

    fengx
    fix auto Visual material

--- VisualFengx/VisualFengx/VisualFileXML.py
+++ VisualFengx/VisualFengx/VisualFileXML.py
@@ -1,10 +1,17 @@
-# -*- coding: utf-8 -*-
+#coding=utf-8
 from  xml.etree.ElementTree import*
 import sys
 import string
 import os
-test="E:/mf_pangu/tw2/res/scene/common/jz/jzsj/sjyw/bghm_jzsj_yw0010_010.visual"
 
+try:
+    os.remove('setMaterials LOG'+'.fengx')
+except:
+    print ""
+
+
+ROOTDIR="E:/mf_pangu/tw2/res/"
+LOG=""
 def getDNS(Dpath):
     paths=[]
     paths.append(Dpath)
@@ -13,65 +20,29 @@ def getDNS(Dpath):
     name=os.path.splitext(os.path.split(Dpath)[1])[0]
     nameParts=string.split(name,"_")
     if len(nameParts)>3:
+        isaOn=False
         typeName=nameParts[len(nameParts)-2]
-        tempID=2
+        tempID=len(nameParts)-2
         if nameParts[len(nameParts)-2]=="a":
             typeName=nameParts[len(nameParts)-3]
-            tempID=3
-        tempStr=""
-        for ts in typeName:
-            if ts=="d":
-                tempStr+="n"
-            else:        
-                tempStr+=ts
-        #typeName.replace("d", "n") 
-        typeName=tempStr
-        #print typeName
-        #"n"
-        newName=""
-        i=0
-        for s in nameParts:
-            if i==(len(nameParts)-tempID):
-                newName+=(typeName+"_")
-                #print newName
-            else:
-                if i==(len(nameParts)-1):
-                    newName+=s
-                    #print newName
-                else:
-                    if s!="a":
-                        newName+=(s+"_")
-                        #print newName
-            i+=1
-        Npath=tempPath+"/"+newName+tempType
+            tempID=len(nameParts)-3
+            isaOn=True
+        fName=""
+        bName=""
+        for i in range (0,len(nameParts)):
+            aSite=2
+            if isaOn:
+                aSite=3
+
+            if i<(len(nameParts)-aSite):
+                fName+=nameParts[i]+"_"
+            elif i>(len(nameParts)-1):#-aSite):
+                bName+="_"+nameParts[i]
+        Npath=tempPath+fName+"n"+bName+tempType
+        Spath=tempPath+fName+"s"+bName+tempType
         paths.append(Npath)
-        #"s"
-        tempStr=""
-        for ts in typeName:
-            if ts=="n":
-                tempStr+="s"
-            else:        
-                tempStr+=ts
-        #typeName.replace("d", "s") 
-        typeName=tempStr
-        #print typeName
-        newName=""
-        i=0
-        for s in nameParts:
-            if i==(len(nameParts)-tempID):
-                newName+=(typeName+"_")
-                #print newName
-            else:
-                if i==(len(nameParts)-1):
-                    newName+=s
-                    #print newName
-                else:
-                    if s!="a":
-                        newName+=(s+"_")
-                        #print newName
-            i+=1
-        Spath=tempPath+"/"+newName+tempType
         paths.append(Spath)
+
         return paths
 
 def indent(elem, level=0):
@@ -88,47 +59,87 @@ def indent(elem, level=0):
     return elem
 
 def setMaterials(VPath,outVPath):   
+    global LOG
     vf=ElementTree(file=VPath)
     v=ElementTree(file=VPath).getroot()
     
     #dump(v)
     #shaderPath=v.find("renderSet/geometry/primitiveGroup/material/fx").text
     mateID=v.findall("renderSet/geometry/primitiveGroup")
+
+    changeMapOn=True
+    miPaths=[]
+    miRootPath="E:/mf_pangu/tw2/res/mi/"
+
+
+    #-----------only support defult export visual
+
+    #----get mi dirs
+    for p in os.listdir(miRootPath):
+        miPaths.append((p+"/"))
+    for p in os.listdir(miRootPath+"common/"):
+        miPaths.append(("common/"+p+"/"))
+    
+
     for mid in mateID:
-        #print mid.text
         #shaderType
-        
         for m in mid:
             m.find("fx").text="	shaders/core/scene_dns.fx	"
             try:
                 m.find("collisionFlags").text
             except:
                 SubElement(m,"collisionFlags").text=" 0 "
-            
             try:
                 m.find("materialKind").text
             except:
                 SubElement(m,"materialKind").text=" 0 "
-    
             i=0
             
             DPath=""
-            if len(m.findall("property"))<3:
+
+            if changeMapOn==True and len(m.findall("property"))<30:
+
                 mapTypes=["	diffuseTexMap","	normalTexMap","	specularTexMap"]
-     
+                
                 for mmap in m.findall("property"):
                     i+=1
                     try:
                         mmap.find("Texture").text
                     except:
-                        print " "
+                        print "  "
                     else:
-                        print "xx"
                         DPath=mmap.find("Texture").text
+                        LOG+=("\n difusseMap path get from Visual file :   " +DPath)
                         mmap.text=mapTypes[0]
-                 #m.remove(mmap)
-                if DPath!="":
-                    #print str(DPath)
+
+                
+                if DPath!="" and os.path.isfile((ROOTDIR+DPath).replace("\t","")):
+                    LOG+="\n"+"difusseMap 存在 开始找 其他贴图。。。"
+
+                    miOn=False
+                    # check mi 
+                    #----set mi
+                    try:
+                        a=m.find("mi").text
+                        if a=="" or a=="\r":
+                            miOn=True
+                        changeMapOn=False
+                    except:
+                        miOn=True
+                    if miOn:
+                        print "xxxxxxx"
+                        miPath=""
+                        name=os.path.splitext(os.path.split(DPath)[1])[0]
+                        for p in miPaths:
+                            #print (miRootPath+p+"/"+name+".mi")
+                            if os.path.isfile(miRootPath+p+"/"+name+".mi"):
+                                miPath="mi/"+p+name+".mi"  
+                        #print miPath
+
+                        SubElement(m,"mi").text=miPath
+                        LOG+="\n 设置子材质 ： "+miPath
+
+                    #----set normal map and specular map
                     maps=getDNS(DPath)
                     i=0
                     for texP in  maps:
@@ -136,10 +147,13 @@ def setMaterials(VPath,outVPath):
                             item= Element("property")
                             item.text=mapTypes[i]
                             texN=Element("Texture")
-                            texN.text=texP
+                            if os.path.isfile((ROOTDIR+texP)):
+                                texN.text=texP
+                            else: texN.text=" "
                             item.append(texN)
                             m.append(item)
                         i+=1
+
             if DPath!="":
                 name=os.path.splitext(os.path.split(DPath)[1])[0]
                 nameParts=string.split(name,"_")
@@ -161,28 +175,32 @@ def setMaterials(VPath,outVPath):
     outV = ElementTree()
     outV._setroot(indent(v))
     outV.write(outVPath,"utf-8")
-#setMaterials("01.visual","test.xml")
+
 
 def replaceVisual(oldPath):
     tempPath="fengx.temp"
     setMaterials(oldPath,tempPath)
+
+
     os.remove(oldPath)
+
+    #LOG+="\n 无法移除"+oldPath
     os.rename(tempPath,oldPath)
 
-#replaceVisual("01.visual")#("E:\GitHub\VisualFengx_3dmax\VisualFengx\VisualFengx\01 -sads.visual")
+
+
 try:
     sys.argv[1]
 except:
     raw_input("don't click me ! dray file up me ! ")
 else:
-    #print "all\n"
-    #f=sys.argv[1]
-    #print f
+    replaceVisual(sys.argv[1])
+    l = open('setMaterials LOG'+'.fengx', 'w')
+    l.write(LOG)
+    l.close()
     #raw_input()
 
-	replaceVisual(sys.argv[1])
 
-		
-
+#test="E:\\mf_pangu\\tw2\\res\\scene\\judian\\yhys_layout0080_010.visual"
 #replaceVisual(test)
 #raw_input()
